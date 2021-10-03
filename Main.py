e64f7fb7ee70712be094eb17b1d72df629dab98c
[Thisara Hettikankanama] <[thisarayashodagcc@gmail.com]>
2021-10-03 19:02:58 +0530

    add config file

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ import time
 #load_dotenv()
 
 #bot_token = os.getenv('API_KEY')
-bot_token=os.environ.get('API_KEY')
+bot_token=os.environ.get('API_KEY',"")
 
 bot=telebot.TeleBot(token=bot_token,parse_mode=None)
 
